10746f423d3ff121cb55d1c041087de56754cf25
JimLynchCodes <mrdotjim@gmail.com>
2020-05-08 16:38:20 -0400

    close but no cigars

--- run-script.sh
+++ run-script.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 # Build your stuff with the input arguments
+
+echo "$0"
+echo $0
+echo $1
+echo $2
+echo $3
+echo $#
+
+
 npm_parameters=""
-npm_separator="--"
-for var in "$@"
+
+if [ "$#" -eq "0" ]; then
+   npm start
+   exit
+fi
+
+for i in {1..$#}
 do 
-  npm_parameters="$npm_parameters $npm_separator$var"
+  npm_parameters="$npm_parameters $i"
 done
+
+eval npm start -- $npm_parameters
+
+
+# for var in "$@"
+# do 
+#   npm_parameters="$npm_parameters $npm_separator$var"
+# done
 # Then call your npm start
-eval npm start $npm_parameters
